970f1398329b9004789c169e98690d2526bca587
h <xf0ece@gmail.com>
2021-09-28 18:11:38 +0300

    clock

--- src/03_javascript/Project/Canvas/CameraMan.js
+++ src/03_javascript/Project/Canvas/CameraMan.js
@@ -22,9 +22,22 @@ export default class CameraMan {
   tick(t) {
     t *= 0.0004
 
+    if(innerWidth < 1024) {
+      this.mobileMove(t)
+    } else {
+      this.desktopMove(t)
+    }
+  }
+
+
+  mobileMove(t) {
     this.camera.position.z = this.camera.userData.z - (this.camera.userData.z - this.yarBox.mesh.scale.z * 0.6) * this.start
     this.camera.position.x = this.yarBox.mesh.scale.x * 0.01 * this.start
     this.camera.position.y = this.yarBox.mesh.scale.y * -0.2 * this.start
+  }
+
+  desktopMove(t) {
+    this.mobileMove(t)
     this.camera.rotation.z = Math.sin(t) * 0.02
     this.camera.rotation.x = Math.cos(t ) * 0.01
     this.camera.rotation.y = Math.sin(t * 2) * 0.01
